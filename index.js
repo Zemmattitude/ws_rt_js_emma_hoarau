5e42ac10508791d67460c46353b66b875169ecb8
Zemmattitude <emma-frederique.hoarau@epitech.eu>
2024-02-02 16:33:16 +0400

    step 1-5

--- index.js
+++ index.js
@@ -28,11 +28,25 @@ function drawRectangle(x, y, width, height, color, size) {
     ctx.closePath();
 }
 
+function drawCircle(x, y, r) {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var X = x;
+    var Y = y;
+    var R = r;
+    ctx.beginPath();
+    ctx.arc(X, Y, R, 2 * Math.PI, false);
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+}
+
 function start() {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     ctx.fillStyle = "red";
     ctx.fillRect(0, 0, 380, 280);
     ctx.lineWidth = 10;
-    drawRectangle(10, 10, 20, 40, "black", 1);
+    drawRectangle(0, 0, 380, 280, "black", 20);
+    //drawCircle(100, 200, 50);
 }
